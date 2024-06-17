93328e76d0e1fec18a84ddd9f0031f48eac9fe8e
ParallaX12 <saalim.araf@gmail.com>
2024-06-17 18:57:31 +0600

    add functionality to delete blogs

--- client/src/components/Blogs/AllBlogsCards.jsx
+++ client/src/components/Blogs/AllBlogsCards.jsx
@@ -1,41 +1,48 @@
 import { useLocation } from "react-router-dom";
 import HeaderCard from "../Shared/HeaderCard";
-import { useContext, useEffect, useState,  } from "react";
+import { useContext, useEffect, useState } from "react";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { AuthContext } from "../../Auth/AuthProvider";
+import { MessageContext } from "../../pages/Root";
 
 const AllBlogsCards = () => {
     const location = useLocation();
     const [blogs, setBlogs] = useState([]);
     const axiosSecure = useAxiosSecure();
     const { userInfo } = useContext(AuthContext);
-    const [loading , setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const {notifySuccess, notifyError} = useContext(MessageContext);
 
     useEffect(() => {
         if (location.pathname === "/blogs/explore") {
             setLoading(true);
-            axiosSecure.get("/blogs/1/allblogs").then((response) => {
-                setBlogs(response.data.blogs);
-                console.log(response.data.blogs);
-            }).finally(() => {
-                setLoading(false);
-            });
-
+            axiosSecure
+                .get("/blogs/1/allblogs")
+                .then((response) => {
+                    setBlogs(response.data.blogs);
+                    console.log(response.data.blogs);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
         }
 
         if (location.pathname === "/blogs/my-blogs" && userInfo._id) {
             // fetch user blogs
             setLoading(true);
-            axiosSecure.get(`/blogs/${userInfo._id}/users`).then((response) => {
-                setBlogs(response.data.blogs);
-                console.log(response.data.blogs);
-            }).then(() => {
-                setLoading(false);
-            });
+            axiosSecure
+                .get(`/blogs/${userInfo._id}/users`)
+                .then((response) => {
+                    setBlogs(response.data.blogs);
+                    console.log(response.data.blogs);
+                })
+                .then(() => {
+                    setLoading(false);
+                });
         }
     }, [location]);
 
-    if(loading) {
+    if (loading) {
         return (
             <div className="flex justify-center items-center mt-10">
                 <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
@@ -43,19 +50,39 @@ const AllBlogsCards = () => {
         );
     }
 
+    const handleDelete = (blogid) => {
+        setLoading(true);
+        axiosSecure
+            .delete(`/blogs/${blogid}`)
+            .then((response) => {
+                console.log(response.data);
+                setBlogs(blogs.filter((blog) => blog.blog._id !== blogid));
+                notifySuccess("Blog deleted successfully");
+            })
+            .catch((error) => {
+                console.error(error);
+                notifyError("Failed to delete blog");
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
     return (
         <div className="flex flex-col items-center mt-10 space-y-3">
-            { blogs.map((blog) => {
+            {blogs.map((blog) => {
                 return (
                     <HeaderCard
                         key={blog.blog._id}
                         title={blog.blog.title}
                         author={blog.user.name}
+                        authorid={blog.user._id}
                         date={blog.blog.updated_at || blog.blog.created_at}
-                        id={blog.blog._id}
+                        blogid={blog.blog._id}
                         likes={blog.blog.likes}
                         comments={blog.blog.comments}
                         authorProfilePic={blog.user.profilepic}
+                        handleDelete={handleDelete}
                     />
                 );
             })}
--- client/src/components/Shared/HeaderCard.jsx
+++ client/src/components/Shared/HeaderCard.jsx
@@ -2,12 +2,21 @@ import { BiComment } from "react-icons/bi";
 import { FaThumbsUp } from "react-icons/fa6";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../Auth/AuthProvider";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
-const HeaderCard = ({author, title, date, id, likes, comments, authorProfilePic}) => {
-    console.log(author, title, date, id);
+const HeaderCard = ({author, title, date, blogid, likes, comments, authorProfilePic, authorid, handleDelete}) => {
+    console.log(author, title, date, blogid, likes, comments, authorProfilePic, authorid);
+    const axiosSecure = useAxiosSecure();
+
+    
+
+
+    const { userInfo } = useContext(AuthContext);
     return (
-        <Link to={`/b/${id}`} className="flex flex-col max-w-4xl p-5 border-2 border-gray-200 w-full space-y-4 rounded-lg">
-            <div className="flex items-center justify-between">
+        <div className="flex flex-col max-w-4xl p-5 border-2 border-gray-200 w-full space-y-4 rounded-lg">
+            <Link to={`/b/${blogid}`} className="flex items-center justify-between">
                 <div className="flex items-center">
                     <img
                         className="rounded-full size-14"
@@ -21,20 +30,27 @@ const HeaderCard = ({author, title, date, id, likes, comments, authorProfilePic}
                 <h1 className="font-bold text-xl">
                     {`${title}`.substring(0, 80)}
                 </h1>
-            </div>
-            {/* blog title */}
+            </Link>
             {/* likes and comments */}
-            <div className="flex items-center">
-                <div className="flex items-center mr-5">
-                    <BiComment className="mr-1" />
-                    <p>{comments.length}</p>
-                </div>
+            <div className="flex items-center justify-between">
                 <div className="flex items-center">
-                    <FaThumbsUp className="mr-1" />
-                    <p>{likes.length}</p>
+                    <div className="flex items-center mr-5">
+                        <BiComment className="mr-1" />
+                        <p>{comments.length}</p>
+                    </div>
+                    <div className="flex items-center">
+                        <FaThumbsUp className="mr-1" />
+                        <p>{likes.length}</p>
+                    </div>
                 </div>
+                {/* delete button */}
+                {
+                    userInfo._id === authorid && (
+                        <button className="text-white text-sm hover:cursor-pointer font-bold bg-red-500 px-2 py-1 rounded-2xl" onClick={() => handleDelete(blogid)}>Delete</button>
+                    )
+                }
             </div>
-        </Link>
+        </div>
     );
 };
 
@@ -42,10 +58,12 @@ HeaderCard.propTypes = {
     author: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
+    blogid: PropTypes.string.isRequired,
     likes: PropTypes.array.isRequired,
     comments: PropTypes.array.isRequired,
     authorProfilePic: PropTypes.string.isRequired,
+    authorid: PropTypes.string.isRequired,
+    handleDelete: PropTypes.func.isRequired,
 };
 
 export default HeaderCard;
