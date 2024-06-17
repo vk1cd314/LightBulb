c6dfda07dec332674556cbbd04185d4b97eef590
ParallaX12 <saalim.araf@gmail.com>
2024-06-17 21:15:38 +0600

    connect profile information to db

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,73 +1,86 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Auth/AuthProvider";
 import ProfileStats from "../components/ProfileStats";
 import { Link } from "react-router-dom";
-
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import Communities from './Communities';
 
 const Profile = () => {
     const { userInfo } = useContext(AuthContext);
+    console.log(userInfo);
+    const axiosSecure = useAxiosSecure();
+    const [profileData, setProfileData] = useState({});
 
-
-    
+    useEffect(() => {
+        axiosSecure.get(`/users/${userInfo._id}/userdata`).then((response) => {
+            console.log(response.data);
+            setProfileData(response.data);
+        });
+    }, []);
 
     return (
-        <div className=" max-w-4xl mx-auto ">
-            <div className="flex flex-col gap-10 h-dvh items-center justify-center">
+        <div className=" max-w-5xl mx-auto mt-10">
+            <div className="flex flex-col gap-10 min-h-dvh items-center justify-center">
                 <div className="flex gap-10">
-                    <div className="p-10 bg-accent rounded-xl text-white min-h-96">
+                    <div className="p-10 bg-accent rounded-xl text-white min-h-96 max-w-96">
                         <div className="flex gap-5 text-xl">
                             {/* user profile image */}
                             <img
-                                src={userInfo.profilepic}
+                                src={profileData?.user?.profilepic}
                                 alt="profile"
                                 className="rounded-full w-32 h-32"
                             />
-                            {/* replace with actual username from db */}
-                            <p>@{userInfo.username}</p>
+                            <p>@{profileData?.user?.username}</p>
                         </div>
                         {/* user display name */}
                         <p className="text-2xl font-medium mt-3">
-                            {userInfo.name}
+                            {profileData?.user?.name}
                         </p>
                         <p className="text-xl font-medium">About:</p>
                         {/* replace with actual about from db */}
-                        <p>{userInfo.about}</p>
+                        <p>{profileData?.user?.about}</p>
                     </div>
-                    <div className="grid grid-cols-2 gap-5">
-                        {/* replace with stats for each */}
-                        <ProfileStats
-                            title={"Followers"}
-                            stat={9860}
-                        ></ProfileStats>
-                        <ProfileStats
-                            title={"Following"}
-                            stat={13435}
-                        ></ProfileStats>
-                        <ProfileStats
-                            title={"Communities"}
-                            stat={531}
-                        ></ProfileStats>
-                        <ProfileStats title={"Blogs"} stat={832}></ProfileStats>
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="grid grid-cols-2 gap-5">
+                            {/* replace with stats for each */}
+                            <ProfileStats
+                                title={"Followers"}
+                                stat={profileData?.followers?.length}
+                            ></ProfileStats>
+                            <ProfileStats
+                                title={"Following"}
+                                stat={profileData?.following?.length}
+                            ></ProfileStats>
+                            <ProfileStats
+                                title={"Communities"}
+                                stat={profileData?.communities?.length}
+                            ></ProfileStats>
+                            <ProfileStats
+                                title={"Blogs"}
+                                stat={profileData?.blogs?.length}
+                            ></ProfileStats>
+                        </div>
+                         {/* buttons for drafts and my communities */}
+                        <div className="my-5">
+                            <button className="bg-primary text-white px-5 py-2 rounded-lg hover:bg-accent">
+                                My Drafts
+                            </button>
+                            <Link
+                                to="/communities/my-communities"
+                                className="bg-primary text-white px-5 py-2 rounded-lg ml-5 hover:bg-accent"
+                            >
+                                My Communities
+                            </Link>
+                            <Link
+                                to="/blogs/my-blogs"
+                                className="bg-primary text-white px-5 py-2 rounded-lg ml-5 hover:bg-accent"
+                            >
+                                My Blogs
+                            </Link>
+                        </div>
                     </div>
                 </div>
-                {/* buttons for drafts and my communities */}
-                <div>
-                    <button className="bg-primary text-white px-5 py-2 rounded-lg hover:bg-accent">
-                        My Drafts
-                    </button>
-                    <Link
-                        to="/communities/my-communities"
-                        className="bg-primary text-white px-5 py-2 rounded-lg ml-5 hover:bg-accent"
-                    >
-                        My Communities
-                    </Link>
-                    <Link
-                        to="/blogs/my-blogs"
-                        className="bg-primary text-white px-5 py-2 rounded-lg ml-5 hover:bg-accent"
-                    >
-                        My Blogs
-                    </Link>
-                </div>
+               
             </div>
         </div>
     );
