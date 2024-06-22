ef984585650b2ed56f19c42b6a55278940e82f8b
ParallaX12 <saalim.araf@gmail.com>
2024-06-22 21:02:03 +0600

    Refactor Drafts to use TanStack Query

--- client/src/pages/Drafts.jsx
+++ client/src/pages/Drafts.jsx
@@ -3,53 +3,53 @@ import useAxiosSecure from "../hooks/useAxiosSecure";
 import { AuthContext } from "../Auth/AuthProvider";
 import { Link } from "react-router-dom";
 import { MessageContext } from "./Root";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import Loader from "../components/FunctionalComponents/Loader";
 
 const Drafts = () => {
     const axiosSecure = useAxiosSecure();
-    const [drafts, setDrafts] = useState([]);
-    const [isloading, setIsLoading] = useState(false);
     const { userInfo, loading } = useContext(AuthContext);
     const { notifySuccess, notifyError } = useContext(MessageContext);
 
-    useEffect(() => {
-        if (!loading) {
-            setIsLoading(true);
+    const queryClient = useQueryClient();
+
+    const { data: drafts, isLoading } = useQuery({
+        queryKey: ["drafts", userInfo._id],
+        queryFn: () =>
             axiosSecure
                 .get(`/drafts/${userInfo._id}/drafts`)
-                .then((response) => {
-                    console.log(response.data);
-                    setDrafts(response.data);
-                })
-                .catch((error) => {
-                    console.error(error);
-                })
-                .finally(() => {
-                    setIsLoading(false);
-                });
-        }
-    }, []);
+                .then((res) => res.data),
+        enabled: !!userInfo._id,
+    });
 
-    if (isloading || loading) {
+    const deleteMutation = useMutation({
+        mutationFn: (id) => axiosSecure.delete(`/drafts/${id}`),
+        onSuccess: () => {
+            notifySuccess("Draft deleted successfully");
+            queryClient.invalidateQueries(["drafts", userInfo._id]);
+        },
+        onError: () => {
+            notifyError("Failed to delete draft");
+        },
+    });
+
+    if (isLoading || loading) {
+        return <Loader />;
+    }
+
+    if (drafts.length === 0) {
         return (
-            <div className="flex items-center justify-center h-96">
-                <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+            <div className="text-center mt-20 min-h-[calc(100dvh-200px)] flex flex-col justify-center items-center">
+                <p className="text-2xl font-bold">No drafts found</p>
+                <Link
+                    to="/blog/create"
+                    className="hover:bg-accent bg-primary text-white p-2 rounded-lg mt-5"
+                >
+                    Create a new blog
+                </Link>
             </div>
         );
     }
-
-    const handleDelete = (id) => {
-        axiosSecure
-            .delete(`/drafts/${id}`)
-            .then((response) => {
-                console.log(response.data);
-                setDrafts(drafts.filter((draft) => draft._id !== id));
-                notifySuccess("Draft deleted successfully");
-            })
-            .catch((error) => {
-                console.error(error);
-                notifyError("Failed to delete draft");
-            });
-    };
 
     return (
         <div className="mt-20 max-w-3xl mx-auto min-h-dvh">
@@ -59,27 +59,32 @@ const Drafts = () => {
                         <th className="border-2 border-gray-500">Title</th>
                         <th className="border-2 border-gray-500">Created At</th>
                         <th className="border-2 border-gray-500">Action</th>
-
                     </tr>
                 </thead>
                 <tbody>
-                    {drafts.map((draft) => (
-                        <tr key={draft._id}>
-                            <td className="border-2 border-gray-500 underline underline-offset-4 p-2">
-                                <Link to={`/drafts/${draft._id}/edit`}>
-                                    {draft.title}
-                                </Link>
-                            </td>
-                            <td className="border-2 border-gray-500 p-2">
-                                {draft.created_at}
-                            </td>
-                            <td className="border-2 border-gray-500 p-2">
-                                <button className="bg-red-500 text-white p-2 rounded-lg hover:bg-red-600" onClick={()=>handleDelete(draft._id)}>
-                                    Delete
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
+                    {drafts &&
+                        drafts.map((draft) => (
+                            <tr key={draft._id}>
+                                <td className="border-2 border-gray-500 underline underline-offset-4 p-2">
+                                    <Link to={`/drafts/${draft._id}/edit`}>
+                                        {draft.title}
+                                    </Link>
+                                </td>
+                                <td className="border-2 border-gray-500 p-2">
+                                    {draft.created_at}
+                                </td>
+                                <td className="border-2 border-gray-500 p-2">
+                                    <button
+                                        className="bg-red-500 text-white p-2 rounded-lg hover:bg-red-600"
+                                        onClick={() =>
+                                            deleteMutation.mutate(draft._id)
+                                        }
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
                 </tbody>
             </table>
         </div>
